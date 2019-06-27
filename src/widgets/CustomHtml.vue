bc1d0cdd813e8ca9c7bd28285718d2453b93bd13
Khanh Doan <admin@khanh.pro>
2019-06-27 13:18:20 +0700

    new widgets

---
+++ src/widgets/CustomHtml.vue
@@ -0,0 +1,24 @@
+<template>
+  <div :class="'custom-html ' + displayClass" v-html="html"></div>
+</template>
+
+<script type="text/javascript">
+import mixin from './display-option-mixin';
+
+export default {
+
+  name: 'custom-html',
+
+  mixins: [mixin],
+
+  data() {
+    return {
+      html: ''
+    };
+  },
+
+  created() {
+    this.html = this.options.html.value;
+  }
+}
+</script>
---
+++ src/widgets/RatingStars.vue
@@ -0,0 +1,49 @@
+<template>
+  <div :class="'rating-stars ' + displayClass">
+    <!-- Review stars -->
+    <div class="mt-1" style="height: 1.5 rem">
+      <div style="display: inline-block;">
+        <div class="mr-3" style="color: #ffab00"> 
+          
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star-half-alt"></i>
+
+        </div>
+      </div>
+      <div style="display: inline-block;">
+        <p> <a href="#" class="text-muted">{{ rating }} - {{ totalReviews }} reviews</a></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script type="text/javascript">
+import mixin from './display-option-mixin';
+
+export default {
+
+  name: 'rating-stars',
+
+  props: ['options'],
+
+  mixins: [mixin],
+
+  data() {
+
+    return {
+      rating: 4.8,
+      totalReviews: 688
+    };
+
+  },
+
+  created() {
+    this.rating = this.options.rating.value;
+    this.totalReviews = this.options.totalReviews.value;
+  }
+
+}
+</script>
